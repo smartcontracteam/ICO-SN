4893f349642300edcf9c9cb6bc831c695a7db521
SilentNotary <SilentNotary@gmail.com>
2017-07-15 19:46:16 +0300

    Add files via upload

---
+++ dapp/src/SilentNotary.sol
@@ -0,0 +1,78 @@
+pragma solidity ^0.4.11;
+
+import './common/ownership/Ownable.sol';
+import './common/token/ERC20.sol';
+
+/// @title SilentNotary contract - store SHA-384 file hash in blockchain
+/// @author dev@smartcontracteam.com
+contract SilentNotary is Ownable {
+	uint public price;
+	address public token;
+
+	struct Entry {
+		uint blockNumber;
+		uint timestamp;
+	}
+
+	mapping (bytes32 => Entry) public entryStorage;
+
+  event EntryAdded(bytes32 hash, uint blockNumber, uint timestamp);
+  event EntryExistAlready(bytes32 hash, uint timestamp);
+
+	/// Fallback method
+	function () {
+      // If ether is sent to this address, send it back
+      throw;
+	}
+
+	/// @dev Set price in SNTR tokens for storing
+	/// @param _price price in SNTR tokens
+	function setRegistrationPrice(uint _price) onlyOwner {
+		price = _price;
+  }
+
+	/// @dev Set SNTR token address
+	/// @param _token Address SNTR tokens contract
+ 	function setTokenAddress(address _token) onlyOwner {
+		token = _token;
+  }
+
+	/// @dev Register file hash in contract, web3 integration
+	/// @param hash SHA-384 file hash
+  function makeRegistration(bytes32 hash) onlyOwner public {
+			makeRegistrationInternal(hash);
+  }
+
+	/// @dev Payable registration in SNTR tokens
+	/// @param hash SHA-384 file hash
+	function makePayableRegistration(bytes32 hash) public {
+			address sender = msg.sender;
+      ERC20 token = ERC20(tokenAddress);
+      uint amount = token.allowance(sender, owner);
+      if(amount == 0) throw;
+      if(!token.transferFrom(sender, owner, price)) throw;
+			makeRegistrationInternal(hash);
+	}
+
+	/// @dev Internal registation method
+	/// @param hash SHA-384 file hash
+  function makeRegistrationInternal(bytes32 hash) internal {
+			uint timestamp = now;
+      // Checks documents isn't already registered
+      if (exist(hash)) {
+          EntryExistAlready(hash, timestamp);
+          throw;
+      }
+      // Registers the proof with the timestamp of the block
+      entryStorage[hash] = Entry(block.number, timestamp);
+      // Triggers a EntryAdded event
+      EntryAdded(hash, block.number, timestamp);
+  }
+
+	/// @dev Check hash existance
+	/// @param hash SHA-384 file hash
+	/// @return Returns true if hash exist
+  function exist(bytes32 hash) internal constant returns (bool) {
+      return entryStorage[hash].blockNumber != 0;
+	}
+}
---
+++ dapp/src/SilentNotaryToken.sol
@@ -0,0 +1,209 @@
+pragma solidity ^0.4.11;
+
+import '/src/common/lifecycle/Killable.sol';
+import '/src/common/token/ERC20.sol';
+import '/src/common/SafeMath.sol';
+import '/src/TokenHolders.sol';
+
+ /// @title SilentNotaryToken contract - standard ERC20 token with Short Hand Attack and approve() race condition mitigation.
+ /// @author dev@smartcontracteam.com
+contract SilentNotaryToken is SafeMath, TokenHolders, ERC20, Killable {
+  string public name = "Silent Notary Token";
+  string public symbol = "SNTR";
+  uint public decimals = 8;
+
+  /// Minimum amount for buyout in wei
+  uint buyOutAmount;
+  /// Buyout price in wei
+  uint buyOutPrice;
+
+  /// contract that is allowed to create new tokens and allows unlift the transfer limits on this token
+  address public crowdsaleAgent;
+  /// A crowdsale contract can release us to the wild if ICO success. If false we are are in transfer lock up period.
+  bool public released = false;
+  /// approve() allowances
+  mapping (address => mapping (address => uint)) allowed;
+
+  /// @dev Limit token transfer until the crowdsale is over.
+  modifier canTransfer() {
+    if(!released) {
+        if(msg.sender != crowdsaleAgent) {
+            throw;
+        }
+    }
+    _;
+  }
+
+  /// @dev The function can be called only before or after the tokens have been releasesd
+  /// @param _released token transfer and mint state
+  modifier inReleaseState(bool _released) {
+    if(_released != released) {
+        throw;
+    }
+    _;
+  }
+
+  /// @dev The function can be called only by release agent.
+  modifier onlyCrowdsaleAgent() {
+    if(msg.sender != crowdsaleAgent) {
+        throw;
+    }
+    _;
+  }
+
+  /// @dev Fix for the ERC20 short address attack http://vessenes.com/the-erc20-short-address-attack-explained/
+  /// @param size payload size
+  modifier onlyPayloadSize(uint size) {
+     if(msg.data.length < size + 4) {
+       throw;
+     }
+     _;
+  }
+
+  /// @dev Make sure we are not done yet.
+  modifier canMint() {
+     if(released) throw;
+     _;
+   }
+
+  /// Tokens burn event
+  event Burned(address indexed burner, address indexed holder, uint burnedAmount);
+  /// Tokens buyout event
+  event Pay(address indexed to, uint value);
+  /// Wei deposit event
+  event Deposit(address indexed from, uint value);
+
+  /// @dev Constructor
+  /// @param _buyOutPrice Buyout price
+  /// @param _buyOutAmount  Buyout limit
+  function SilentNotaryToken(uint _buyOutPrice, uint _buyOutAmount) {
+    owner = msg.sender;
+    buyOutPrice = _buyOutPrice;
+    buyOutAmount = _buyOutAmount;
+  }
+
+  /// Fallback method will buyout tokens
+  function() payable {
+    buyout();
+  }
+  /// @dev Create new tokens and allocate them to an address. Only callably by a crowdsale contract
+  /// @param receiver Address of receiver
+  /// @param amount  Number of tokens to issue.
+  function mint(address receiver, uint amount) onlyCrowdsaleAgent canMint public {
+      totalSupply = safeAdd(totalSupply, amount);
+      addOrUpdate(receiver, safeAdd(balances.data[receiver].value, amount));
+      Transfer(0, receiver, amount);
+  }
+  /// @dev Set minimum limit for buyout
+  /// @param _buyOutAmount min buyout amount
+  function setBuyoutMinAmount(uint _buyOutAmount) onlyOwner public {
+    buyOutAmount = _buyOutAmount;
+  }
+  /// @dev Set the contract that can call release and make the token transferable.
+  /// @param _crowdsaleAgent crowdsale contract address
+  function setCrowdsaleAgent(address _crowdsaleAgent) onlyOwner inReleaseState(false) public {
+    crowdsaleAgent = _crowdsaleAgent;
+  }
+  /// @dev One way function to release the tokens to the wild. Can be called only from the release agent that is the final ICO contract. It is only called if the crowdsale has been success (first milestone reached).
+  function releaseTokenTransfer() public onlyCrowdsaleAgent {
+    released = true;
+  }
+  /// @dev Tranfer tokens to address
+  /// @param _to dest address
+  /// @param _value tokens amount
+  /// @return transfer result
+  function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) canTransfer returns (bool success) {
+    addOrUpdate(msg.sender, safeSub(balances.data[msg.sender].value, _value));
+    addOrUpdate(_to, safeAdd(balances.data[_to].value, _value));
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /// @dev Tranfer tokens from one address to other
+  /// @param _from source address
+  /// @param _to dest address
+  /// @param _value tokens amount
+  /// @return transfer result
+  function transferFrom(address _from, address _to, uint _value) canTransfer returns (bool success) {
+    uint _allowance = allowed[_from][msg.sender];
+    addOrUpdate(_to, safeAdd(balances.data[_to].value, _value));
+    addOrUpdate(_from, safeSub(balances.data[_from].value, _value));
+    allowed[_from][msg.sender] = safeSub(_allowance, _value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+  /// @dev Tokens balance
+  /// @param _owner holder address
+  /// @return balance amount
+  function balanceOf(address _owner) constant returns (uint balance) {
+    return balances.data[_owner].value;
+  }
+
+  /// @dev Approve transfer
+  /// @param _spender holder address
+  /// @param _value tokens amount
+  /// @return result
+  function approve(address _spender, uint _value) returns (bool success) {
+    // To change the approve amount you first have to reduce the addresses`
+    //  allowance to zero by calling `approve(_spender, 0)` if it is not
+    //  already 0 to mitigate the race condition described here:
+    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) throw;
+
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /// @dev Token allowance
+  /// @param _owner holder address
+  /// @param _spender spender address
+  /// @return remain amount
+  function allowance(address _owner, address _spender) constant returns (uint remaining) {
+    return allowed[_owner][_spender];
+  }
+
+  /// @dev Internal burn
+  /// @param _holder holder address
+  /// @param _amount burn amount
+  function burn(address _holder, uint _amount) internal  {
+      balances.data[_holder].value = safeSub(balances.data[_holder].value, _amount);
+      totalSupply = safeSub(totalSupply, _amount);
+      Burned(msg.sender, _holder, _amount);
+  }
+
+  /// @dev Internal buyout method
+  function buyout() internal  {
+    Deposit(msg.sender, msg.value);
+
+    uint totalSupplyAmount = totalSupply;
+    uint payoutBalance = safeAdd(this.balance, msg.value);
+    if(payoutBalance == 0)
+       return;
+
+    if(safeMul(totalSupplyAmount, buyOutPrice) <= payoutBalance)
+       buyOutAmount = 0;
+
+    if(payoutBalance < buyOutAmount)
+        return;
+
+    for (var i = iterateStart(); iterateValid(i); i = iterateNext(i)) {
+      var current = iterateGet(i);
+      if(balances.data[current.key].value == 0) {
+          continue;
+      }
+
+      uint multiplier = 10 ** decimals;
+      uint holderTokenPortion =  safeDiv(safeMul(balances.data[current.key].value, multiplier), totalSupplyAmount);
+      uint holderWeiPortion  = safeDiv(safeMul(holderTokenPortion, payoutBalance), multiplier);
+
+      if(holderWeiPortion < buyOutPrice)
+          continue;
+
+      uint holderBuyoutTokens =  safeDiv(holderWeiPortion, buyOutPrice);
+      burn(current.key, holderBuyoutTokens);
+      current.key.transfer(holderWeiPortion);
+      Pay(current.key, holderWeiPortion);
+    }
+  }
+}
