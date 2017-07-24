198b5548b9ac480c7594cc3f8da2fc6e954b7255
SilentNotary <SilentNotary@gmail.com>
2017-07-24 13:27:53 +0300

    Update Migrations.sol

--- dapp/src/common/lifecycle/Migrations.sol
+++ dapp/src/common/lifecycle/Migrations.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.13;
 
 import "../ownership/Ownable.sol";
 
@@ -16,3 +16,4 @@ contract Migrations is Ownable {
     upgraded.setCompleted(lastCompletedMigration);
   }
 }
+
