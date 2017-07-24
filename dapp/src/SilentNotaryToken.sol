fd2267bc6334c5aa155ba18d3797474aa4ca40be
SilentNotary <SilentNotary@gmail.com>
2017-07-24 13:32:35 +0300

    Update SilentNotaryToken.sol

--- dapp/src/SilentNotaryToken.sol
+++ dapp/src/SilentNotaryToken.sol
@@ -1,32 +1,32 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.13;
 
 import '/src/common/lifecycle/Killable.sol';
 import '/src/common/token/ERC20.sol';
 import '/src/common/SafeMath.sol';
 
 /*
-This code is in the testing stage and may contain certain bugs. 
+This code is in the testing stage and may contain certain bugs.
 These bugs will be identified and eliminated by the team at the testing stage before the ICO.
 Please treat with understanding. If you become aware of a problem, please let us know by e-mail: service@silentnotary.com.
-If the problem is critical and security related, we will credit you with the reward from the team's share in the tokens 
+If the problem is critical and security related, we will credit you with the reward from the team's share in the tokens
 at the end of the ICO (as officially announced at bitcointalk.org).
 Thanks for the help.
 */
 
-/// @title SilentNotaryToken contract - standard ERC20 token with Short Hand Attack and approve() race condition mitigation.
+ /// @title SilentNotaryToken contract - standard ERC20 token with Short Hand Attack and approve() race condition mitigation.
  /// @author dev@smartcontracteam.com
 contract SilentNotaryToken is SafeMath, ERC20, Killable {
   string constant public name = "Silent Notary Token";
   string constant public symbol = "SNTR";
   uint constant public decimals = 4;
 
-  /// Minimum amount for buyout in wei
-  uint buyOutAmount;
-  /// Buyout price in wei
-  uint buyOutPrice;
+  /// Minimum amount for starting buyout logic in wei
+  uint public minBuyOutAmount = 0;
+  /// Buyout price
+  uint constant public buyOutPrice = 200 finney;
   /// Holder list
   address[] holders;
-  /// Balance data 
+  /// Balance data
   struct Balance {
     /// Tokens amount
     uint value;
@@ -62,7 +62,6 @@ contract SilentNotaryToken is SafeMath, ERC20, Killable {
     if(!balances[holder].exist)
       holders.push(holder);
     _;
-
   }
 
   /// @dev The function can be called only by release agent.
@@ -92,19 +91,13 @@ contract SilentNotaryToken is SafeMath, ERC20, Killable {
   event Deposit(address indexed from, uint value);
 
   /// @dev Constructor
-  /// @param _buyOutPrice Buyout price
-  /// @param _buyOutAmount  Buyout limit
-  function SilentNotaryToken(uint _buyOutPrice, uint _buyOutAmount) {
-    require(_buyOutPrice > 0);
-    require(_buyOutAmount > 0);
-
+  function SilentNotaryToken() {
     owner = msg.sender;
-    buyOutPrice = _buyOutPrice;
-    buyOutAmount = _buyOutAmount;
   }
 
   /// Fallback method will buyout tokens
   function() payable {
+    require(msg.value > 0);
     buyout();
   }
   /// @dev Create new tokens and allocate them to an address. Only callably by a crowdsale contract
@@ -116,10 +109,10 @@ contract SilentNotaryToken is SafeMath, ERC20, Killable {
       balances[receiver].exist = true;
       Transfer(0, receiver, amount);
   }
-  /// @dev Set minimum limit for buyout
-  /// @param _buyOutAmount min buyout amount
-  function setBuyoutMinAmount(uint _buyOutAmount) onlyOwner public {
-    buyOutAmount = _buyOutAmount;
+  /// @dev Set minimum limit for starting buyout logic
+  /// @param _minBuyOutAmount min buyout amount
+  function setBuyoutMinAmount(uint _minBuyOutAmount) onlyOwner public {
+    minBuyOutAmount = _minBuyOutAmount;
   }
   /// @dev Set the contract that can call release and make the token transferable.
   /// @param _crowdsaleAgent crowdsale contract address
@@ -147,7 +140,7 @@ contract SilentNotaryToken is SafeMath, ERC20, Killable {
   /// @param _to dest address
   /// @param _value tokens amount
   /// @return transfer result
-  function transferFrom(address _from, address _to, uint _value) canTransfer addIfNotExist(_to) returns (bool success) {
+  function transferFrom(address _from, address _to, uint _value) onlyPayloadSize(2 * 32) canTransfer addIfNotExist(_to) returns (bool success) {
     var _allowance = allowed[_from][msg.sender];
 
     balances[_to].value = safeAdd(balances[_to].value, _value);
@@ -156,6 +149,7 @@ contract SilentNotaryToken is SafeMath, ERC20, Killable {
 
     allowed[_from][msg.sender] = safeSub(_allowance, _value);
     Transfer(_from, _to, _value);
+    return true;
   }
   /// @dev Tokens balance
   /// @param _owner holder address
@@ -173,7 +167,7 @@ contract SilentNotaryToken is SafeMath, ERC20, Killable {
     //  allowance to zero by calling `approve(_spender, 0)` if it is not
     //  already 0 to mitigate the race condition described here:
     //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) throw;
+    require((_value == 0) && (allowed[msg.sender][_spender] == 0));
 
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
@@ -192,38 +186,39 @@ contract SilentNotaryToken is SafeMath, ERC20, Killable {
   /// @param _holder holder address
   /// @param _amount burn amount
   function burn(address _holder, uint _amount) internal  {
+      address burner = msg.sender;
       balances[_holder].value = safeSub(balances[_holder].value, _amount);
       totalSupply = safeSub(totalSupply, _amount);
-      Burned(msg.sender, _holder, _amount);
+      Burned(burner, _holder, _amount);
+      /// Keep exchanges happy by sending the burned amount to
+      /// "burn address"
+      Transfer(burner, 0x0, _amount);
   }
 
   /// @dev Internal buyout method
   function buyout() internal  {
     Deposit(msg.sender, msg.value);
+    uint balance = safeAdd(this.balance, msg.value);
 
-    uint totalSupplyAmount = totalSupply;
-    uint payoutBalance = safeAdd(this.balance, msg.value);
-
-    if(safeMul(totalSupplyAmount, buyOutPrice) <= payoutBalance)
-       buyOutAmount = 0;
-
-    if(payoutBalance < buyOutAmount)
+    /// if amount less than min limit will store ether in contract
+    if(balance < minBuyOutAmount)
         return;
 
-    for (var i = 0; i < holders.length; i++) {
+    uint multiplier = 10 ** decimals;
+    uint totalSupplyInt = safeDiv(totalSupply, multiplier);
+
+    for (uint i = 0; i < holders.length; i++) {
       var holder = holders[i];
 
-      uint multiplier = 10 ** decimals;
-      uint holderTokenPortion =  safeDiv(safeMul(balances[holder].value, multiplier), totalSupplyAmount);
-      uint holderWeiPortion  = safeDiv(safeMul(holderTokenPortion, payoutBalance), multiplier);
+      uint tokenPortion = safeDiv(balances[holder].value, totalSupplyInt);
+      uint weiPortion = safeDiv(safeMul(tokenPortion, balance), multiplier);
 
-      if(holderWeiPortion < buyOutPrice)
-          continue;
-
-      uint holderBuyoutTokens =  safeDiv(holderWeiPortion, buyOutPrice);
-      burn(holder, holderBuyoutTokens);
-      holder.transfer(holderWeiPortion);
-      Pay(holder, holderWeiPortion);
+      if(weiPortion > 0 && weiPortion >= buyOutPrice) {
+        uint buyoutTokens = safeDiv(safeMul(weiPortion, multiplier), buyOutPrice);
+        burn(holder, buyoutTokens);
+        holder.transfer(weiPortion);
+        Pay(holder, weiPortion);
+      }
     }
   }
 }
