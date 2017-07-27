05ce5195b0fb31c596509e058f34fcf929f8b796
smartcontracteam <mail@smartcontracteam.com>
2017-07-27 22:55:03 +0300

    Update ERC20.sol

--- dapp/src/common/token/ERC20.sol
+++ dapp/src/common/token/ERC20.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.13;
 
  /// @title ERC20 interface see https://github.com/ethereum/EIPs/issues/20
- /// @author dev@smartcontracteam.com
 contract ERC20 {
   uint public totalSupply;
   function balanceOf(address who) constant returns (uint);
